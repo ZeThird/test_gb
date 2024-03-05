bab338785aa5c086e632fdb76d9043c565c6716c
ZeThird <levbatura@rambler.ru>
2024-03-05 23:53:35 +0800

    finally finished the part of converting input to str array through regex and printing it

--- Program.cs
+++ Program.cs
@@ -1,5 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Text.RegularExpressions;
+using System;
 
+static string[] arr_from_str(string input_str, string sep=" ") {
+    string regex_pattern_str = $"[^({sep})]+"; 
+    Regex regex_pattern = new Regex(regex_pattern_str);
+    MatchCollection words_in_input = regex_pattern.Matches(input_str);
+    int regex_match_number = words_in_input.Count;
+    string[] result_arr = new string[regex_match_number];
+    for (int i = 0 ; i < regex_match_number ; i++) {
+        result_arr[i] = words_in_input[i].Value;
+    }
+    return result_arr;
+}
 
-Console.WriteLine("Hello, World!");
+string arr_input = Console.ReadLine();
+Console.WriteLine(string.Join(" ", arr_from_str(arr_input)));
